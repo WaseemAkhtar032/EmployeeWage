21b0b498c0bf956dd364c056d4f36484579503f9
WaseemAkhtar032 <113421575+WaseemAkhtar032@users.noreply.github.com>
2022-10-07 08:43:22 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" WELCOME TO EMPLOYEE WAGE COMPUTATION \n ");
+        SwitchCase s1 = new Switchcase();
+        s1.Switchcase();
     }
 }
